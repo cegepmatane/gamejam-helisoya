edbeda04ee3347987fa3f46ef3ba6f926f3615fa
valentin-mn <97812645+valentin-mn@users.noreply.github.com>
2022-12-02 22:17:19 -0500

    Adding bigfoot health

--- Assets/Scripts/Bigfoot/BigfootController.cs
+++ Assets/Scripts/Bigfoot/BigfootController.cs
@@ -1,20 +1,27 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigfootController : MonoBehaviour
+
+public class BigfootController : NetworkBehaviour
 {
+
+    //[SerializeField] Bullet bullet;
 
     [SerializeField] private float speed;
 
     [SerializeField] private float rotationSpeed;
 
+    public int maxHealth = 50;
+    [SyncVar] public int currentHealth;
 
-    // Start is called before the first frame update
-    void Start()
+    public override void OnStartServer()
     {
+        currentHealth = maxHealth;
         
     }
+
 
     // Update is called once per frame
     void Update()
@@ -33,5 +40,42 @@ public class BigfootController : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(20);
+            print(currentHealth);
+        }
     }
+
+    
+    [Command(requiresAuthority =false)]
+    public void TakeDamage(int dammage)
+    {
+        currentHealth -= dammage;
+
+        HesDead();
+    }
+
+    public void HesDead()
+    {
+        if (currentHealth <= 0)
+        {
+            print("le bigfoot est mort");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        
+       // if (Input.GetKeyDown(KeyCode.P))
+        //{
+            Debug.LogWarning("collision ennemi");
+
+            TakeDamage(20);
+        //}
+    }
+
+
 }
