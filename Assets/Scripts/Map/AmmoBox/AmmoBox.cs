9343b9a677e354eab67f6225a43358385e5d0025
helisoya <oudot.julien@laposte.net>
2022-12-04 15:17:32 -0500

    fixed ammo again

--- Assets/Scripts/Map/AmmoBox/AmmoBox.cs
+++ Assets/Scripts/Map/AmmoBox/AmmoBox.cs
@@ -32,13 +32,7 @@ public class AmmoBox : NetworkBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Player") && collision.GetComponent<HunterMovement>().isLocalPlayer)
-        {
-            HunterMovement.localPlayer.AddAmmoFromBox(this);
-        }
-    }
+
 
 
 }
